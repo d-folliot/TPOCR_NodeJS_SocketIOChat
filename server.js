eaf51d231900b2018863acbc458cc6a6f84c6c11
d-folliot <53329638+d-folliot@users.noreply.github.com>
2020-04-07 13:39:34 +0200

    Exercice maintenant complet
    Cette fois-ci, ça marche au niveau des session.
    OCR link un module qui est deprecated.
    Ce module link lui même un autre module pour le remplacer, qui est aussi deprecated. le bon module pour gérer les sessions avec socket.io et express actuallement est express-socket.io-session.
    -Corrigé bugs visuel
    -Changé l'affichage des lignes
    -Corrigé bug sur Edge (non prise en compte de scrollTo() ).

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
-
-const app = require ('express')(); 
+const express = require ('express');
+const app = express(); 
 const http = require('http').createServer(app);
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const sharedsession = require('express-socket.io-session'); // la SEULE VRAI SOLUTION QUI MARCHE ACTUELLEMENT POUR LES SESSIONS SOUS SOCKET.IO
+const session =  require("express-session")({
+    secret: "TresSecretDefense",
+    resave: true,
+    saveUninitialized: true
+  });
 const io = require ('socket.io')(http);
-//const sessionStore = require('sessionstore');
-//const sessionSockets = new SessionSockets(io, sessionStore, cookieParser);
-//session({secret: "TresSecretDefence"});
-
 
 app
-.use(cookieParser())
-.use(session({secret:'TresSecretDefense', saveUninitialized: false, resave:false}))
-.get('/chat', function(req, res){
-    console.log(req.session)
-    res.setHeader('Content-Type','text/html' );
-    res.status(200)
-    .render('chat.ejs'); 
-})
-.use (function(req, res){
-    res.setHeader('Content-Type', 'text/html' );
-    res.status(404).render('er404.ejs');
-});
-
-
-io.on('connection', function (socket) {
-    console.log('a user connected');
-
-    socket.broadcast.emit('chat message', 'a user connected');
-    
-    socket.on('chat message', function (info) {
-        console.log('message by '+ session.pseudo + " : " + info);
-        if (info != "" &&  !info.match( /^\s+$/)) 
-            socket.broadcast.emit('chat message', [session.pseudo, info]); 
-    });
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-        socket.broadcast.emit('chat message', 'a user disconnected');
-    });
-    socket.on('identity', function(pseudo){
-        console.log(pseudo);
-        session.pseudo= pseudo;        
+    .use(cookieParser())
+    .use(session)
+    .get('/chat', function(req, res){    
+        res.setHeader('Content-Type','text/html' );
+        res.status(200).render('chat.ejs'); 
+    })
+    .use (function(req, res){
+        res.setHeader('Content-Type', 'text/html' );
+        res.status(404).render('er404.ejs');
     });
 
-});
+io.
+    use(sharedsession(session, { // pour utiliser les sessions sous express-socket.io-session
+        autoSave:true
+    }))
+    .on('connection', function (socket) { 
+        socket.on('chat message', function (info) {
+            if (info != "" &&  !info.match( /^\s+$/)) 
+                socket.broadcast.emit('chat message', [socket.handshake.session.pseudo, info]); // on recup ensuite les variables de sessions dans socket.handshake.session
+        });
+        socket.on('disconnect', function () {
+            socket.broadcast.emit('chat message',['', socket.handshake.session.pseudo + ' s\'est déconnecté']);
+        });
+        socket.on('identity', function(pseudo){
+            socket.handshake.session.pseudo=pseudo;   
+            socket.handshake.session.save(); 
+            socket.broadcast.emit('chat message', ['', socket.handshake.session.pseudo + ' s\'est connecté']);   
+        });
+
+    });
 
 http.listen(8080);
